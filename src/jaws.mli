2e1ef5fa9587612b650fa3091638fa2cbfa68ad3
Matthew Ost <most1@jhu.edu>
2021-11-18 16:40:32 -0500

    Added initial dune files

--- src/jaws.mli
+++ src/jaws.mli
@@ -1,7 +1,7 @@
 open Owl
 
 (* Given a dataframe of a player's career data, compute their peak statistics. *)
-val compute_peak_statistics : Dataframe.t -> peak_size:int ->  Dataframe.t
+val compute_peak_statistics : Dataframe.t -> peak_size:int -> Dataframe.t
 
 (* For a query player, get players with similar peak stats. *)
 val get_nearby_players : string -> Dataframe.t
